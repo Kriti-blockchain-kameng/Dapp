02ea3bef13333e80781d70a227f6fe7ab1aa18d9
Priyansh-2 <priyanshmathur02@gmail.com>
2024-02-10 23:51:57 +0530

    frontend

--- src/components/Doctors.tsx
+++ src/components/Doctors.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import DoctorsComponent from './DoctorsComponent'
 
 const Doctors = () => {
   return (
-    <div>
-        
-    </div>
+    <div className='container px-[3vw] py-[3vh] '>
+   <h3 className='font-semibold'>Select available doctor</h3>
+   <p className='text-sm'>Allergy and Immunology</p>
+  <DoctorsComponent/>
+  </div>
   )
 }
 
